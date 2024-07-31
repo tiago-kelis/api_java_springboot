2835a98fc6b1e26719832040ddee6bd40464fed9
Tiago kelis <tiagokelis2017@gmail.com>
2024-07-31 03:15:59 -0300

    version 2

--- api/src/main/java/br/com/projeto/api/Controler/Controle.java
+++ api/src/main/java/br/com/projeto/api/Controler/Controle.java
@@ -3,16 +3,61 @@ package br.com.projeto.api.Controler;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.modelo.Pessoa;
+import br.com.projeto.api.repositorio.Repositorio;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @RestController
-public class Controle {   
+public class Controle { 
+
+    @Autowired
+    private Repositorio acao;
+    @PostMapping("/api")
+    public Pessoa cadastrar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
+    @GetMapping("/api")
+    public List<Pessoa> selecionar(){
+        return acao.findAll();
+    }
+
+    @GetMapping("/api/{codigo}")
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo){
+       return acao.findById(codigo);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+    public void deletar(@PathVariable int codigo){
+
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
+    }
+
+    @GetMapping("/api/contador")    
+    public long Contador(){
+        return acao.count();
+    }
+
+    // @GetMapping("/api/order")
+    // public List<Pessoa> orderPorNome(){
+    //     return acao.findByOrderByIdade();
+    // }
     
     @GetMapping("")
     public String mensagem(){
--- api/src/main/java/br/com/projeto/api/repositorio/Repositorio.java
+++ api/src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -1,5 +1,8 @@
 package br.com.projeto.api.repositorio;
 
+import java.util.List;
+
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,12 @@ import br.com.projeto.api.modelo.Pessoa;
 
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
+
+    @SuppressWarnings("null")
+    List<Pessoa> findAll();
+
+   Pessoa findById(int codigo);
+
+    // List<Pessoa> findByOrderByIdade();
     
 }
